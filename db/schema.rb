f840d58eb705ebebdee7d9ce5da989566ebb38b4
Carlos Eugenio <carlos.eugenio@usach.cl>
2016-06-03 22:11:39 -0400

    Working on articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603231803) do
+ActiveRecord::Schema.define(version: 20160604012613) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "creator"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
